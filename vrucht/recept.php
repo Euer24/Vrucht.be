fcbfeedca4cb591626e320b548608eae866a84a4
Euer <euer2017@gmail.com>
2023-11-06 17:08:07 +0100

    Vrucht website dag 1
    + basis paginas aangemaakt
    +bijna klaar met hompagina

---
+++ vrucht/recept.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+<header class="header">
+        <a href="#" class="logo">Logo</a>
+
+    <nav class="navbar">
+                <a href="home.html">Home</a>
+                <a href="recept.php">Recepten</a>
+                <a href="overons.html">Over Ons</a>
+                <a href="contact.html">Contact</a>
+    </nav>
+</header>
+</body>
+</html>
