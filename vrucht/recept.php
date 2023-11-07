1872252bb15472d3f99ae84f5a73ab9885156f92
Euer <euer2017@gmail.com>
2023-11-07 17:03:53 +0100

    Recepten pagina met descriptie af

--- vrucht/recept.php
+++ vrucht/recept.php
@@ -5,10 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/flexbox.css">
 </head>
 <body>
 <header class="header">
-        <a href="#" class="logo">Logo</a>
+<img src="/img/logo.png" class="logo"alt="logo" >
 
     <nav class="navbar">
                 <a href="home.html">Home</a>
@@ -17,5 +18,41 @@
                 <a href="contact.html">Contact</a>
     </nav>
 </header>
+
+<?php
+include 'namen.php';
+?>
+
+<h1>De nieuwste recepten</h1>
+<br><br><br>
+<div class="flex-container">
+
+<div class="flex-item"><img src="img/img9.jpg" alt="img 9"><a href="recepten/recept1.php"><?php echo "$recept1"; ?> </a><br>
+<p> 6-11-2023</p></div>
+
+<div class="flex-item"><img src="img/img10.jpg" alt="img 10"><a href="recepten/recept2.php"><?php echo "$recept2"; ?> </a><br>
+<p>6-11-2023</p></div>
+
+<div class="flex-item"><img src="img/img11.jpg" alt="img 11"><a href="recepten/recept3.php"><?php echo "$recept3"; ?> </a><br>
+ <p>6-11-2023</p></div>
+
+ <div class="flex-item"><img src="img/img4.jpg" alt="img 4"><a href="recepten/recept4.php"><?php echo "$recept4"; ?> </a><br>
+<p>3-10-2023</p></div>
+
+<div class="flex-item"><img src="img/img5.jpg" alt="img 5"><a href="recepten/recept5.php"><?php echo "$recept5"; ?> </a><br>
+<p>3-10-2023</p></div>
+
+<div class="flex-item"><img src="img/img6.jpg" alt="img 6"><a href="recepten/recept6.php"><?php echo "$recept6"; ?> </a><br>
+<p>1-8-2023</p></div>
+
+<div class="flex-item"><img src="img/img7.jpg" alt="img 7"><a href="recepten/recept7.php"><?php echo "$recept7"; ?> </a><br>
+<p>1-8-2023</p></div>
+
+<div class="flex-item"><img src="img/img8.jpg" alt="img 8"><a href="recepten/recept8.php"><?php echo "$recept8"; ?> </a><br>
+<p>1-8-2023</p></div>
+</div>
+
+
 </body>
 </html>
+
---
+++ vrucht/recepten/recept3.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/flexbox.css">
+<style>
+
+body {
+    background-image: linear-gradient(rgb(144, 238, 144), rgb(0, 191, 255));
+}
+
+    .recept-container {
+            width: 100%;
+            max-width: 500px; 
+            margin: 0 auto; 
+            background-color: #fff;
+            padding: 20px;
+            box-shadow: 0 0 10px rgba(0,0,0,0.1);
+            border-radius: 10px;
+        }
+  
+        .recept-container img {
+            width: 100%;
+            height: auto; 
+        }
+
+        p {
+            text-align: left;
+            margin-left: 170px;
+        }
+
+        h1 {
+            text-align: center;
+            color: #333;
+        }
+
+        h2 {
+            text-align: center;
+            color: #666;
+        }
+
+        .colum {
+            border: 1px solid #000;
+            padding: 10px;
+        }
+
+        .button {
+            margin-top: 120px;
+            margin-left: 250px;
+            background-color:teal;
+            border: none;
+            color: white;
+            padding: 10px 20px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 16px;
+            border-radius: 6px;
+        }
+
+        .button:hover {
+            background-color:  #006666;;
+         
+        }
+    </style>
+
+</head>
+<body>
+<?php include '../namen.php'; ?>
+
+<a href="../recept.php" class="button">Ga Terug</a>
+
+    <div class="recept-container">
+        <img src="../img/img11.jpg">
+
+        <div class="colum">
+
+        <h1>ingrediënten</h1>
+        
+       <h2> <?php echo "$recept3"; ?></h2>
+        
+        <p>Handje diepvries mango</p>
+        <p>Handje diepvries framboos</p>
+        <p>Handje diepvries bramen</p>
+        <p>200ml haverdrank </p>
+        </div>
+    </div>
+
+
+
+
+</body>
+
+
+</html>
