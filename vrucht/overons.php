5583c111c2542e11fcf75ea27ea70421d49ce98a
Euer <euer2017@gmail.com>
2023-11-12 12:09:11 +0100

    Update overons.php

---
+++ vrucht/overons.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Overons</title>
+    <?php include 'layouts/head.php'?>
+    <link rel="stylesheet" href="/css/footer.css">
+    <link rel="stylesheet" href="/css/overons.css">
+   
+</head>
+
+<body>
+<?php include 'layouts/navbar.php'; ?>
+
+<div class="image-row">
+    <div class="background-image" style="background-image: url('img/people1.jpg');"></div>
+    <div class="background-image" style="background-image: url('img/people2.jpg');"></div>
+    <div class="background-image" style="background-image: url('img/people3.jpg');"></div>
+</div>
+
+<h1>Over ons</h1>
+
+
+<p>Hoi mijn naam is Euer en Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+<p class="last-p">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Magnis dis parturient montes nascetur ridiculus mus. Ipsum suspendisse ultrices gravida dictum fusce ut placerat orci. Quisque non tellus orci ac auctor augue mauris. Morbi quis commodo odio aenean sed adipiscing. Sit amet mauris commodo quis imperdiet massa tincidunt nunc. Eget egestas purus viverra accumsan in nisl nisi scelerisque. Velit dignissim sodales ut eu sem integer vitae. Risus commodo viverra maecenas accumsan lacus vel. Arcu cursus euismod quis viverra nibh cras.
+
+</p>
+
+
+<?php  include 'layouts/footer.php'; ?>
+
+</body>
+</html>
+
