afb00d1aac05ad0ea2158e171ef7c85017054a43
Euer <euer2017@gmail.com>
2023-11-13 17:30:25 +0100

    overal /

--- vrucht/layouts/recept-navbar.php
+++ vrucht/layouts/recept-navbar.php
@@ -7,11 +7,11 @@
     <a href="recept.php" class="dropdown-toggle">Recepten</a>
     <a href="#" class="dropdown-button">&#9660;</a>
     <div class="dropdown-content">
-        <a href="recepten/recept1.php">Mangosmoothie</a>
-        <a href="recepten/recept2.php">Fruitijs</a>
-        <a href="recepten/recept3.php">MangoSalade</a>
-        <a href="recepten/recept4.php">Fruit yogurt</a>
-        <a href="recepten/recept5.php">SterfruitSalade</a>
+        <a href="/recepten/recept1.php">Mangosmoothie</a>
+        <a href="/recepten/recept2.php">Fruitijs</a>
+        <a href="/recepten/recept3.php">MangoSalade</a>
+        <a href="/recepten/recept4.php">Fruit yogurt</a>
+        <a href="/recepten/recept5.php">SterfruitSalade</a>
         <a href="recepten/recept6.php">Watermaloenshake</a>
         <a href="recepten/recept7.php">Fruitblokijs</a>
         <a href="recepten/recept8.php">Fruitspies</a>
