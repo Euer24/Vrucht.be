8589d771f85ff288abf75a1c781f6e255a64ef04
Euer <euer2017@gmail.com>
2023-11-24 13:11:13 +0100

    test

--- vrucht/recept.php
+++ vrucht/recept.php
@@ -59,54 +59,7 @@
         21-10-2023
         </p>
     </div>
-    <div class="flex-item">
-        <a href="/recepten/recept5">
-            <img src="/public/img/img5.webp" alt="img 9">
-        </a>
-        <a href="/recepten/recept5" class="titel">
-           Sterfruitsalade
-        </a>
-        <br>
-        <p>
-        6-10-2023
-        </p>
-    </div>
-    <div class="flex-item">
-        <a href="/recepten/recept6">
-            <img src="/public/img/img6.webp" alt="img 9">
-        </a>
-        <a href="/recepten/recept6" class="titel">
-           Watermeloen shake
-        </a>
-        <br>
-        <p>
-        6-10-2023
-        </p>
-    </div>
-    <div class="flex-item">
-        <a href="/recepten/recept7">
-            <img src="/public/img/img7.webp" alt="img 9">
-        </a>
-        <a href="/recepten/recept7" class="titel">
-            Fruit ijs
-        </a>
-        <br>
-        <p>
-        6-10-2023
-        </p>
-    </div>
-    <div class="flex-item">
-        <a href="/recepten/recept8">
-            <img src="/public/img/img8.webp" alt="img 9">
-        </a>
-        <a href="/recepten/recept8" class="titel">
-            Fruit spies
-        </a>
-        <br>
-        <p>
-        6-10-2023
-        </p>
-    </div>
+
 
 <?php include $_SERVER['DOCUMENT_ROOT'] . '/public/layouts/footer.php'; ?>
 <?php include $_SERVER['DOCUMENT_ROOT'] . '/public/layouts/hamburger.php'; ?>
