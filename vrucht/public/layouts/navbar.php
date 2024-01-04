29294cf45eed96eaf07405c5f5db14d84490793c
Euer <euer2017@gmail.com>
2024-01-04 16:04:06 +0100

    navbar links blijven nu aligend

--- vrucht/public/layouts/navbar.php
+++ vrucht/public/layouts/navbar.php
@@ -12,8 +12,6 @@
                     <a href="/recepten/Cheesecake">Passievrucht/Mango Cheesecake</a>
                     <a href="/recepten/Muffins">Bosbessen-Havermout Muffins</a>
                     <a href="/recepten/Salade">Meloen-Munt Salade</a>
-                    <a href="/over-ons">Over ons</a>
-                    <a href="/contact">Contact</a>
                 </div>
             </div>
             <a href="/over-ons">Over ons</a>
